dd247de5b3252f26271be0b618b253a9631ea3a2
namke-dev <namke.dev@gmail.com>
2023-12-27 01:49:07 +0700

    create navbar

--- src/component/Feed.jsx
+++ src/component/Feed.jsx
@@ -1,7 +1,35 @@
+import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
 
-const Feed = () => {
-  return <div>Feed</div>;
-};
+const Feed = () => (
+  <Stack
+    sx={{
+      flexDirection: {
+        sx: "column",
+        md: "row",
+      },
+    }}
+  >
+    <Box
+      sx={{
+        height: {
+          sx: "auto",
+          md: "92vh",
+        },
+        borderRight: "1px solid #3d3d3d",
+        px: { sx: 0, md: 2 },
+      }}
+    >
+      SideBar
+      <Typography
+        className="Coppyright"
+        variant="body2"
+        sx={{ mt: 1.5, color: "#FFF" }}
+      >
+        Coppyright 2023 NKM Media
+      </Typography>
+    </Box>
+  </Stack>
+);
 
 export default Feed;
