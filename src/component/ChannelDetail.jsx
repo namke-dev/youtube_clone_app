ffa763073eac90816f3193a180ab092e2f3d481b
namke-dev <namke.dev@gmail.com>
2023-12-27 05:30:55 +0700

    implement fetching channel data & channel videos

--- src/component/ChannelDetail.jsx
+++ src/component/ChannelDetail.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchFromApi } from "../utils/fetchFromApi";
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [channelVideos, setChannelVideos] = useState(null);
+  const { id } = useParams();
+
+  console.log(channelDetail, channelVideos);
+
+  useEffect(() => {
+    fetchFromApi(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromApi(`search?channelid=${id}&part=snippet&order=date`).then(
+      (data) => setChannelVideos(data?.items)
+    );
+  }, [id]);
+
   return <div>ChannelDetail</div>;
 };
 
