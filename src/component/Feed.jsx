d42a28b809aca59e6e0aef291e26a7b09653e649
namke-dev <namke.dev@gmail.com>
2023-12-27 04:30:26 +0700

    apply fetch api to Feed component

--- src/component/Feed.jsx
+++ src/component/Feed.jsx
@@ -1,55 +1,73 @@
 import { Box, Stack, Typography } from "@mui/material";
-import React from "react";
-import SideBar from "./SideBar";
-const Feed = () => (
-  <Stack
-    sx={{
-      flexDirection: {
-        sx: "column",
-        md: "row",
-      },
-    }}
-  >
-    <Box
+import React, { useEffect } from "react";
+import { SideBar, Videos } from "../component";
+import { FetcherFormProps } from "react-router-dom";
+import { fetchFromApi } from "../utils/fetchFromApi";
+import { useState } from "react";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
+  return (
+    <Stack
       sx={{
-        height: {
-          sx: "auto",
-          md: "92vh",
+        flexDirection: {
+          sx: "column",
+          md: "row",
         },
-        borderRight: "1px solid #3d3d3d",
-        px: { sx: 0, md: 2 },
       }}
     >
-      <SideBar />
-      <Typography
-        className="Coppyright"
-        variant="body2"
-        sx={{ mt: 1.5, color: "#FFF" }}
-      >
-        Coppyright 2023 NKM Media
-      </Typography>
-    </Box>
-    <Box
-      p={2}
-      sx={{
-        overflowY: "auto",
-        height: "90vh",
-        flex: "2",
-      }}
-    >
-      <Typography
-        variant="h4"
-        fontWeight="bold"
-        mb={2}
+      <Box
         sx={{
-          color: "white",
+          height: {
+            sx: "auto",
+            md: "92vh",
+          },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
         }}
       >
-        New <span style={{ color: "#F31503" }}>videos</span>
-      </Typography>
-      {/* <Videos /> */}
-    </Box>
-  </Stack>
-);
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="Coppyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#FFF" }}
+        >
+          Coppyright 2023 NKM Media
+        </Typography>
+      </Box>
+      <Box
+        p={2}
+        sx={{
+          overflowY: "auto",
+          height: "90vh",
+          flex: "2",
+        }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{
+            color: "white",
+          }}
+        >
+          {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
 
 export default Feed;
