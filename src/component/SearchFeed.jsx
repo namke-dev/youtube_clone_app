38a728bcc463cc3d1d05a3725a8b62d7fd364080
namke-dev <namke.dev@gmail.com>
2023-12-27 08:09:05 +0700

    Perform search Video by searchTerm

--- src/component/SearchFeed.jsx
+++ src/component/SearchFeed.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import { useEffect } from "react";
+import { Videos } from "../component";
+import { fetchFromApi } from "../utils/fetchFromApi";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const { searchTerm } = useParams();
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box
+      p={2}
+      sx={{
+        overflowY: "auto",
+        height: "90vh",
+        flex: "2",
+      }}
+    >
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        mb={2}
+        sx={{
+          color: "white",
+        }}
+      >
+        Search result for <span style={{ color: "#F31503" }}>{searchTerm}</span>{" "}
+        videos
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
