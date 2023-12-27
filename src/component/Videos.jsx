6d3b7a1074f9f7ad3bd9ea2ab99a810ae3ef5a13
namke-dev <namke.dev@gmail.com>
2023-12-27 09:46:28 +0700

    Add dierctinn for Videos Component and Check null parameter videos object

--- src/component/Videos.jsx
+++ src/component/Videos.jsx
@@ -3,15 +3,19 @@ import { Box, Stack } from "@mui/material";
 import ChannelCard from "./ChannelCard";
 import VideoCard from "./VideoCard";
 
-const Videos = (videos) => {
+const Videos = (videos, direction) => {
   videos = videos.videos;
 
-  if (videos === null) {
-    console.log("Videos object is null");
-    return <span>Loading ...</span>;
+  if (!videos?.length) {
+    return "Loading ...";
   }
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
