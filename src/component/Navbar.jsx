5ee721a73f4b1901ec9f5fa8c69d6d390d9b5e38
namke-dev <namke.dev@gmail.com>
2023-12-31 03:33:58 +0700

    format

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -4,19 +4,22 @@ import { logo } from "../utils/constants";
 import SearchBar from "./SearchBar";
 const Navbar = () => (
   <div
-    className="flex flex-row p-2 
-    sticky top-0 
-    bg-white/40
-    backdrop-blur-xl 
-    justify-between
-    text-lg
-    shadow-md"
+    className="
+      flex flex-row p-2 
+      sticky top-0 
+      bg-white/40 
+      backdrop-blur-xl 
+      justify-between items-center 
+      text-lg shadow-md"
   >
     <Link to="/" className="flex items-center font-bold">
-      <img src={logo} alt="logo" className="h-[3rem] mx-3" />
-      DevTube
+      <img src={logo} alt="logo" className="h-8 mx-3" />
+      <span>DevTube</span>
     </Link>
-    <SearchBar className="flex" />
+
+    <div className="flex-grow mx-4 items-center">
+      <SearchBar className="w-full" />
+    </div>
   </div>
 );
 
