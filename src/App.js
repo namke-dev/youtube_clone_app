42fa7e039f558e3eb873907fa70b90c662658cf5
namke-dev <namke.dev@gmail.com>
2024-01-04 16:29:34 +0700

    add some background

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import { VideoDetail, ChannelDetail, Header, HomePage } from "./component";
 import { LoadingProvider } from "./context/loading-context";
 const App = () => (
   <BrowserRouter>
-    <div className="bg-gray-50">
+    <div>
+      <div
+        className="bg-[#fafbd792] -z-10
+          absolute top-[-6rem] right-[11rem] 
+            h-[31.25rem] w-[31.25rem]
+            rounded-full blur-[10rem] 
+            sm:w-[68.75rem]"
+      ></div>
+      <div
+        className="bg-[#e2ecfbaa] -z-10
+            absolute top-[-1rem] left-[-35rem]
+            h-[31.25rem] w-[50rem]
+            rounded-full blur-[10rem] 
+            sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] 
+            xl:left-[-15rem] 2xl:left-[-5rem]"
+      ></div>
       <LoadingProvider>
         <Header />
         <Routes>
