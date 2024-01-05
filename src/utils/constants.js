d82ade32b378f9471b1282b6614c5211ce1047de
namke-dev <namke.dev@gmail.com>
2024-01-05 09:05:38 +0700

    add icon

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,41 +7,38 @@ import MicrowaveIcon from "@mui/icons-material/Microwave";
 import PodcastsIcon from "@mui/icons-material/Podcasts";
 import MusicVideoIcon from "@mui/icons-material/MusicVideo";
 import GraphicEqIcon from "@mui/icons-material/GraphicEq";
-import SportsGymnasticsIcon from "@mui/icons-material/SportsGymnastics";
+import CodeIcon from "@mui/icons-material/Code";
 
 export const logo = window.location.origin + "/logo.png";
 
 export const categories = [
   { name: "Conan", icon: <LiveTvIcon /> },
+  { name: "Thắng", icon: <MusicNoteIcon /> },
+  { name: "Ngọt", icon: <MusicNoteIcon /> },
+  { name: "City Pop", icon: <MusicVideoIcon /> },
   { name: "Làm Bánh", icon: <MicrowaveIcon /> },
+  { name: "Doraemon", icon: <LiveTvIcon /> },
+  { name: "Podcast", icon: <PodcastsIcon /> },
   { name: "Markiplier", icon: <VideoLibraryIcon /> },
   { name: "Ghibli", icon: <MusicNoteIcon /> },
-  { name: "HorrorGame", icon: <SportsEsportsIcon /> },
-  { name: "Thắng", icon: <MusicNoteIcon /> },
-  { name: "Gym", icon: <SportsGymnasticsIcon /> },
-  { name: "Ngọt", icon: <MusicNoteIcon /> },
-  { name: "Podcast", icon: <PodcastsIcon /> },
-  { name: "City Pop", icon: <MusicVideoIcon /> },
+  { name: "Horror Game", icon: <SportsEsportsIcon /> },
   { name: "ASP.NET", icon: <DeveloperModeIcon /> },
   { name: "Indie", icon: <MusicVideoIcon /> },
   { name: "Asmr", icon: <GraphicEqIcon /> },
   { name: "Cozy", icon: <MusicVideoIcon /> },
-  { name: "Cozy1", icon: <MusicVideoIcon /> },
-  { name: "Cozy2", icon: <MusicVideoIcon /> },
-  { name: "Cozy3", icon: <MusicVideoIcon /> },
-  { name: "Cozy4", icon: <MusicVideoIcon /> },
-  { name: "Cozy5", icon: <MusicVideoIcon /> },
-  { name: "Cozy6", icon: <MusicVideoIcon /> },
-  { name: "Cozy7", icon: <MusicVideoIcon /> },
-  { name: "Cozy8", icon: <MusicVideoIcon /> },
-  { name: "Cozy9", icon: <MusicVideoIcon /> },
+  { name: "Rain sound", icon: <MusicVideoIcon /> },
+  { name: "JSMaster", icon: <CodeIcon /> },
+  { name: "React", icon: <CodeIcon /> },
+  { name: "Redux", icon: <CodeIcon /> },
+  { name: "Microservice", icon: <CodeIcon /> },
+  { name: "JavaScript", icon: <CodeIcon /> },
+  { name: ".Net", icon: <CodeIcon /> },
 ];
 
 export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
 export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
 export const demoVideoUrl = "/video/GDa8kZLNhJ4";
 export const demoChannelTitle = "JavaScript Mastery";
-export const demoVideoTitle =
-  "Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI";
+export const demoVideoTitle = "JavaScript & React API Projects";
 export const demoProfilePicture =
   "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
